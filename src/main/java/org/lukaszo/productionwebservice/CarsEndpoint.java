a51ff6348adff9bf005394f7bbd60aecdfabdd31
LukaszOsiniak <lukasz.osiniak@gmail.com>
2021-07-01 10:49:15 +0200

    - Update Added CarsRepository and CarsEndpoint

---
+++ src/main/java/org/lukaszo/productionwebservice/CarsEndpoint.java
@@ -0,0 +1,28 @@
+package org.lukaszo.productionwebservice;
+
+import io.spring.guides.gs_producing_web_service.GetCarRequest;
+import io.spring.guides.gs_producing_web_service.GetCarResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CarsEndpoint {
+
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+    private CarsRepository carsRepository;
+
+    @Autowired
+    public CarsEndpoint(CarsRepository carsRepository){this.carsRepository= carsRepository;}
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCarRequest")
+    @ResponsePayload
+    public GetCarResponse getCar(@RequestPayload GetCarRequest request){
+        GetCarResponse response = new GetCarResponse();
+        response.setCar(carsRepository.findCar(request.getLicensePlateNumber()));
+    return response;
+    }
+}
---
+++ src/main/java/org/lukaszo/productionwebservice/CarsRepository.java
@@ -0,0 +1,54 @@
+package org.lukaszo.productionwebservice;
+
+import io.spring.guides.gs_producing_web_service.Car;
+import io.spring.guides.gs_producing_web_service.FuelType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CarsRepository {
+
+    private static final Map<String, Car> cars =  new HashMap<>();
+
+    @PostConstruct
+    public void initData(){
+        Car firstCar = new Car();
+        firstCar.setName("Hyundai i30");
+        firstCar.setOwner("Mateusz");
+        firstCar.setLicensePlateNumber("RZ 001");
+        firstCar.setYearOfProduction("2019-10-10");
+        firstCar.setFuelType(FuelType.GASOLINE);
+        firstCar.setIdInsured(true);
+
+        cars.put(firstCar.getLicensePlateNumber(), firstCar);
+
+        Car secondCar = new Car();
+        secondCar.setName("Opel Astra");
+        secondCar.setOwner("Kacper");
+        secondCar.setLicensePlateNumber("RZ 002");
+        secondCar.setYearOfProduction("2012-10-10");
+        secondCar.setFuelType(FuelType.DIESEL);
+        secondCar.setIdInsured(true);
+
+        cars.put(secondCar.getLicensePlateNumber(), secondCar);
+
+        Car thirdCar = new Car();
+        thirdCar.setName("Toyota Corolla");
+        thirdCar.setOwner("Krzysztof");
+        thirdCar.setLicensePlateNumber("RZ 003");
+        thirdCar.setYearOfProduction("2009-10-10");
+        thirdCar.setFuelType(FuelType.LPG_AUTOGAS);
+        thirdCar.setIdInsured(false);
+
+        cars.put(thirdCar.getLicensePlateNumber(), thirdCar);
+    }
+
+    public Car findCar(String licensePlateNumber) {
+        Assert.notNull(licensePlateNumber, "The car's license plate number cannot be null");
+        return cars.get(licensePlateNumber);
+    }
+}
